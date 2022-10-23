e20f8e30009557d48ec69cfec3092b0896021afe
nnlnuu <mhw0502@konkuk.ac.kr>
2022-10-24 01:59:10 +0900

    fix/원준 : Modify GameManager.java

--- src/Horse.java
+++ src/Horse.java
@@ -1,58 +1,45 @@
 import java.util.*;
-
 public class Horse {
-    public Pair<Integer, Integer> position; // position
+
+    public Pair<Integer, Integer> position = new Pair<>(0, 0);
     public Stack<Pair<Integer, Integer>> historyStack = new Stack<Pair<Integer, Integer>>(); // 한칸,한칸 마다 기록
 
-    public Horse(){
-        this.position = new Pair<>(6, 6);
-    }
 
     public boolean move(int toMove, char direction) {
 
         // 백도일때 처리
         if (toMove == 0) {
             // historyStack이 비어있을 때
-
-            // 스택 내용 확인
-            for (int i =0; i< historyStack.size(); i++){
-                Pair<Integer, Integer> p = new Pair<>(0,0); // position
-                p = historyStack.elementAt(i);
-                System.out.println(p.first+"," +p.second);
-            }
-
-
-            historyStack.pop();
             if (historyStack.isEmpty()) {
-                Integer i_x = 6, j_y = 6;
+                Integer i_x=6, j_y=6;
 
-                for (j_y = 6; j_y >= 0; j_y--) {
-                    if (j_y == 3)
+                for(j_y=6; j_y >= 0; j_y--) {
+                    if(j_y==3)
                         continue;
-                    historyStack.push(new Pair<>(j_y, i_x));
+                    historyStack.push(new Pair<>(i_x, j_y));
                 }
-                j_y = 0;
-                for (i_x = 5; i_x >= 0; i_x--) {
-                    if (i_x == 3)
+                j_y=0;
+                for(i_x=5; i_x >= 0; i_x--) {
+                    if(i_x==3)
                         continue;
-                    historyStack.push(new Pair<>(j_y, i_x));
+                    historyStack.push(new Pair<>(i_x, j_y));
                 }
-                i_x = 0;
-                for (j_y = 1; j_y <= 6; j_y++) {
-                    if (j_y == 3)
+                i_x=0;
+                for(j_y=1; j_y<=6; j_y++) {
+                    if(j_y==3)
                         continue;
-                    historyStack.push(new Pair<>(j_y, i_x));
+                    historyStack.push(new Pair<>(i_x, j_y));
                 }
-                j_y = 6;
-                for (i_x = 1; i_x <= 6; i_x++) {
-                    if (i_x == 3)
+                j_y=6;
+                for(i_x=1; i_x<=6; i_x++) {
+                    if (i_x==3)
                         continue;
-                    historyStack.push(new Pair<>(j_y, i_x));
+                    historyStack.push(new Pair<>(i_x, j_y));
                 }
 
             }
 
-            position = historyStack.peek();
+            position = historyStack.pop();
 
         }
 
@@ -63,22 +50,24 @@ public class Horse {
         Integer c_y = position.first;   // 현재위치 y좌표
         Integer c_x = position.second;  // 현재위치 x좌표
 
-        /**
+        /*
         알고리즘 설명
         1. toMove
         2. 분기점에서 출발한다면 방향을 고려해서 나아감
         3. 현재 위치에서 방향을 고려해서 나아감
          */
-        System.out.println("이동전 확인"); //todo 이후에 지우기
         while (toMove > 0) {    // 움직일 기회 0되면 종료
+
+
+            historyStack.push(new Pair<>(c_y, c_x));    // 자취 등록
+
             // 현재 위치 int형으로 변환한 값
             y_pos = position.first.intValue();
             x_pos = position.second.intValue();
-            System.out.println(y_pos+":"+x_pos);
+
             // 출발 분기점. 일단 분기점이어서 따로 만듬.
-            if ((y_pos == 6 && x_pos == 6) && historyStack.isEmpty() == true) {
+            if ((y_pos == 6 && x_pos == 6) && historyStack.isEmpty() == true)  {
                 dy = -1;
-                System.out.println("지나가나??"); // todo 지우기
             }
             // 말이 (0,6)에 있는 경우
             else if (y_pos == 0 && x_pos == 6) {
@@ -131,7 +120,7 @@ public class Horse {
 
             // 말이 (6,6)에 있는 경우  // 해당말 끝
             else if (y_pos == 6 && x_pos == 6) {
-               // return true;
+                return true;
             }
 
             // --------------여기서부터 분기점 아닌 칸들-------------
@@ -145,7 +134,7 @@ public class Horse {
             }
             // 위쪽 라인 분기점 제외 4개
             else if (y_pos == 0 && (1 <= x_pos && x_pos <= 5)) {
-                dx = -1;
+                dx -= -1;
             }
             // 아래쪽 라인 분기점 제외 4개
             else if (y_pos == 6 && (1 <= x_pos && x_pos <= 5)) {
@@ -176,7 +165,6 @@ public class Horse {
                 System.out.println("MOVE함수 오류 잘못들어왔습니다! 오류난 좌표" + "(" + position.first + ", " + position.second + ")");
             }
 
-
             x_pos = x_pos + dx;
             y_pos = y_pos + dy;
 
@@ -185,7 +173,7 @@ public class Horse {
                 x_pos = x_pos + dx;
                 y_pos = y_pos + dy;
             }
-            System.out.println("이동중인 좌표 : "+x_pos+","+y_pos);
+
             // 현재 위치 및 남은 이동횟수 업데이트
             position.first = Integer.valueOf(y_pos);
             position.second = Integer.valueOf(x_pos);
@@ -197,8 +185,6 @@ public class Horse {
             direction = 'Z';    // 방향 없애주기 없는 방향은 Z로 함
             c_y = position.first;
             c_x = position.second;
-
-            historyStack.push(new Pair<>(c_y, c_x));    // 자취 등록
         }
 
         return false;   // 말이 나지 않았으므로 false 반환
--- src/Team.java
+++ src/Team.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -8,138 +8,18 @@ public class Team {
     public Set<Horse> groupA;// = new Horse[4];
     public Set<Horse> groupB;// = new Horse[4];
     public boolean[] isEnd;// = new boolean[4];
-    public char moved; // 직전에 움직인 말
-    public int[] yut = new int[6]; // 백도,도,개,걸,윷,모
+    public int yut[] = new int[6]; // 백도,도,개,걸,윷,모
     public int rollCnt; // roll 던질 수 있는 횟수
-    public int teamNum; // 1 팀인지 2팀인지
-    public boolean isTurnEnd;
 
     /**
      * todo : 생성자 작성
      */
-    public Team(int teamNum) {
+    public Team() {
         this.horse = new Horse[4];
-        for(int i  = 0 ;i<4;i++){
-            horse[i] = new Horse();
-        }
-        this.groupA = new HashSet<>();
-        this.groupB = new HashSet<>();
+        this.groupA = new HashSet<Horse>();
+        this.groupB = new HashSet<Horse>();
         this.isEnd = new boolean[4];
-        Arrays.fill(isEnd, false);
         this.rollCnt = 0;
-        this.teamNum = teamNum;
-    }
-
-    /**
-     * controller 에서 받아올 command
-     * move -> move + 어떤말,이동거리,방향
-     * roll -> roll 실행
-     * grouping -> grouping + grouping 할 말들
-     * overloading 으로 controller 구현
-     */
-
-    public int controller(String command) { // roll
-        int yutNum = roll();
-        yut[yutNum]++;
-        return yutNum;
-    }
-
-    public Pair<Integer,Integer> controller(String command, char h, int toMove, char direction) {//move
-        /** 유효성 검사 통과 -> true , 불통 -> false ;
-         *  1. 'A' , 'B' 가 존재하는지?
-         *  2. 들어온 말이 이미 난 말인지?
-         * */
-        System.out.println("move controller 실행"+toMove+" 이동");
-
-        Pair<Integer,Integer> p = new Pair<>(-1,-1);
-        if (h == 'A') {
-            for (int i = 0; i < horse.length; i++) {
-                if (groupA.contains(horse[i])) {
-                    p = horse[i].position;
-                }
-            }
-            if (groupA.isEmpty()) {
-                return new Pair<>(-1,-1);
-            }
-            for (Horse horse1 : groupA) {
-                horse1.move(toMove, direction);
-            }
-        } else if (h == 'B') {
-            for (int i = 0; i < horse.length; i++) {
-                if (groupB.contains(horse[i])) {
-                    p = horse[i].position;
-                }
-            }
-            if (groupB.isEmpty()) {
-                return new Pair<>(-1,-1);
-            }
-            for (Horse horse2 : groupB) {
-                horse2.move(toMove, direction);
-            }
-        } else {
-            p = horse[h-'a'].position;
-            if (isEnd[h - 'a']) {
-                return new Pair<>(-1,-1);
-            }
-            horse[h - 'a'].move(toMove, direction);
-        }
-        moved = h;
-        return p;
-    }
-
-    public char controller(String command, char h1, char h2) {//grouping
-        /**
-         * 유효성 검사
-         * 1. 그룹이 비어있는가?
-         * 2. 그 턴에 움직인 말인지? && 안 움직인 말은 그 자리에 있는가?
-         * 3. 움직였다면 -> 둘다 위치 같은지?
-         * */
-        if (Character.isLowerCase(h1) && Character.isLowerCase(h2)) {           // case 1 말 + 말
-            if (moved == h1 || moved == h2) {
-                if ((horse[h1 - 'a'].position.first.intValue() == horse[h2 - 'a'].position.first.intValue())&&
-                        horse[h1 - 'a'].position.second.intValue() == horse[h2 - 'a'].position.second.intValue()) {
-                    return grouping(h1, h2);
-                }
-            }
-        } else if (Character.isUpperCase(h1) && Character.isUpperCase(h2)) {    // case 3 그룹 + 그룹
-            if (moved == h1 || moved == h2) {
-                Pair<Integer, Integer> p1 = new Pair<>(-1, -1);
-                Pair<Integer, Integer> p2 = new Pair<>(-1, -1);
-                for (int i = 0; i < horse.length; i++) {
-                    if (groupA.contains(horse[i])) {
-                        p1 = horse[i].position;
-                    }
-                    if (groupB.contains(horse[i])) {
-                        p2 = horse[i].position;
-                    }
-                }
-                if (p1.first.intValue() == -1 || p2.first.intValue() == -1) {
-                    System.out.print("p1.first.intValue() == -1 || p2.first.intValue() == -1: ");
-                    return 'Z'; // group 이 비어 있는 경우: false의 의미로 Z를 반환합니다.
-                } else if (p1.first.intValue() == p2.first.intValue() && p1.second.intValue() == p2.second.intValue()) {
-                    return grouping(h1, h2);
-                }
-            }
-        } else {                                                                // case 2 그룹 + 말
-            char g = Character.isUpperCase(h1) ? h1 : h2;
-            char h = Character.isLowerCase(h1) ? h1 : h2;
-            if (moved == h1 || moved == h2) {
-                Pair<Integer, Integer> p = new Pair<>(-1, -1);
-                for (int i = 0; i < horse.length; i++) {
-                    if (g == 'A' ? groupA.contains(horse[i]) : groupB.contains(horse[i])) {
-                        p = horse[i].position;
-                    }
-                }
-                if (p.first.intValue() == -1) {
-                    System.out.print("p.first.intValue() == -1: ");
-                    return 'Z'; // false의 의미로 Z를 반환합니다.
-                } else if (p.first.intValue() == horse[h - 'a'].position.first && p.second.intValue() == horse[h - 'a'].position.second) {
-                    return grouping(h1, h2);
-                }
-            }
-        }
-        System.out.print("Not Grouped: ");
-        return 'Z'; // false의 의미로 Z를 반환합니다.
     }
 
     /**
@@ -147,106 +27,73 @@ public class Team {
      * todo : 윷 확률 결정해주기
      * https://keichee.tistory.com/312 -> ref
      */
-    public int roll() {
-        int yut;
-        Random random = new Random();
-        int num = random.nextInt(10000);
-        if (num < 625)
-            yut = 0;
-        else if (num < 625 + 1875)
-            yut = 1;
-        else if (num < 625 + 1875 + 3750)
-            yut = 2;
-        else if (num < 625 + 1875 + 3750 + 2500)
-            yut = 3;
-        else if (num < 625 + 1875 + 3750 + 2500 + 625) {
-            yut = 4;
-            rollCnt++;
-        } else {
-            yut = 5;
-            rollCnt++;
-        }
-        rollCnt--;
-        printYut(yut);
-        /*int count = 10;
+    public void roll() {
+        int yut = 0;
+        int count = 10;
         for (int i = 0; i < count; i++) {
             Random random = new Random();
             int num = random.nextInt(10000);
-            System.out.println(num + "\n");
+            //System.out.println(num + "\n");
             if (num < 625)
-                yut = 0; // 백도
+                yut = 0;
             else if (num < 625 + 1875)
-                yut = 1; // 도
+                yut = 1;
             else if (num < 625 + 1875 + 3750)
-                yut = 2; // 개
+                yut = 2;
             else if (num < 625 + 1875 + 3750 + 2500)
-                yut = 3; // 걸
+                yut = 3;
             else if (num < 625 + 1875 + 3750 + 2500 + 625)
-                yut = 4; // 윷
+                yut = 4;
             else
-                yut = 5; // 모
+                yut = 5;
             printYut(yut);
-        }*/
-        return yut;
+        }
+
     }
 
     /**
      * 말들이 낫는지를 체크
      */
     public boolean checkIsEnd() {
-        // 하나라도 false 존재 -> 실패
-        for (int i = 0; i < horse.length; i++)
-            if (!isEnd[i]) return false;
-        return true;
+        boolean check = true;
+        // 하나라도 false 존재 -> false;
+        for (int i = 0; i < 4; i++) {
+            if (isEnd[i] == false) check = false;
+        }
+        return check;
     }
 
     /**
      * 실제 그룹 배열
      * 그룹핑 어떻게 할지
      */
-    public char grouping(char h1, char h2) {
-        /** 명령어 검사(GameManager 의 checkCommander) 에서
-         * @args_h1 : 그룹핑할 대상 1
-         * @args_h2 : 그룹핑할 대상 2
-         * @input_case
+    public void grouping(char h1, char h2) {
+        /** 명령어 검사(GameManager의 checkCommander) 에서
+         * 말 또는 그룹 문자를 Parse 한 것을 grouping의 인자로 받는다
+         * 입력의 세 가지 경우:
          * 1. 말 + 말 (둘 다 소문자)-> 비어 있는 그룹에 말 추가
          * 2. 말 + 그룹 / 그룹 + 말 (소문자, 대문자)-> 입력된 그룹에 말 추가
          * 3. 그룹 + 그룹 (둘 다 대문자)-> 그룹을 하나로 병합, 나머지 그룹은 초기화
-         * @author : nnlnuu
          */
-        if (Character.isLowerCase(h1) && Character.isLowerCase(h2)) {           // case 1 말 + 말
+        if (Character.isLowerCase(h1) && Character.isLowerCase(h2)) {           // case 1
             if (groupA.isEmpty()) {
                 groupA.add(horse[h1 - 'a']);
                 groupA.add(horse[h2 - 'a']);
-                return 'A';
             } else {
                 groupB.add(horse[h1 - 'a']);
                 groupB.add(horse[h2 - 'a']);
-                return 'B';
             }
-        } else if (Character.isUpperCase(h1) && Character.isUpperCase(h2)) {    // case 3 그룹 + 그룹
-            groupA.addAll(groupB);
-            groupB.clear();
-            return 'A';
-        } else {                                                                // case 2 말 + 그룹
+        } else if (Character.isUpperCase(h1) && Character.isUpperCase(h2)) {    // case 3
+            while (!groupB.isEmpty()) {
+                groupA.addAll(groupB);
+                groupB.clear();
+            }
+        } else {                                                                // case 2
             char g = Character.isUpperCase(h1) ? h1 : h2;
             char h = Character.isLowerCase(h1) ? h1 : h2;
-            if (g == 'A') {
-                groupA.add(horse[h - 'a']);
-                return 'A';
-            }
-            else {
-                groupB.add(horse[h - 'a']);
-                return 'B';
-            }
+            if (g == 'A')   groupA.add(horse[h - 'a']);
+            else            groupB.add(horse[h - 'a']);
         }
-    }
-
-    //추가했습니다.
-    public void printSrc() {
-        System.out.println("<윷 현황>");
-        System.out.println("백도:" + yut[0] + " 도:" + yut[1] + " 개:" + yut[2] + " 걸:" + yut[3] + " 윷:" + yut[4] + " 모:" + yut[5]);
-       // System.out.println("\n");
     }
 
     public void printYut(int yut) {
@@ -300,6 +147,7 @@ public class Team {
                         "└─┘ └─┘ └─┘ └─┘    ");
                 break;
         }
+
     }
 
 }
